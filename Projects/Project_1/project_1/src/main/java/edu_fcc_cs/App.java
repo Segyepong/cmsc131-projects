0c518c4ae33b245072dd059a759bd12afce8754e
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-02-28 13:31:46 -0500

    Initial Project Structure

---
+++ Projects/Project_1/project_1/src/main/java/edu_fcc_cs/App.java
@@ -0,0 +1,13 @@
+package edu_fcc_cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
