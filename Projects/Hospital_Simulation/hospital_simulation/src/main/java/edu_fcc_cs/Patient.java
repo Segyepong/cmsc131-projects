b850655e3030fb141a008f8fbce474476f8e4cf0
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-03-06 17:11:42 -0500

    Wrote first itteration of Patient and Patient_test.

--- Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Patient.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Patient.java
@@ -1,5 +1,23 @@
 package edu_fcc_cs;
+import java.util.UUID;
 
 public class Patient {
+    private UUID id;
+
+    public Patient(){
+        id = UUID.randomUUID();
+    }
     
+    public UUID getID(){
+        return id;
+    }
+
+    @Override
+    public String toString(){
+        return "Patient ID: "+ id.toString();
+    }
+
+    public static Patient createPatient(){
+        return new Patient();
+    }
 }
--- Projects/Hospital_Simulation/hospital_simulation/src/test/java/edu_fcc_cs/Patient_test.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/test/java/edu_fcc_cs/Patient_test.java
@@ -1,11 +1,33 @@
 package edu_fcc_cs;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 public class Patient_test {
     @Test
-    public void shouldAnswerWithTrue()
+    public void uuid_test()
     {
-        assertTrue( true );
+        Patient test = new Patient();
+
+        assertNotNull(test.getID());
+    }
+
+    @Test
+    public void static_patient_create()
+    {
+        Patient test = Patient.createPatient();
+
+        assertNotNull(test.getID());
+    }
+
+    @Test
+    public void test_tostring()
+    {
+        Patient test = Patient.createPatient();
+        Patient second = Patient.createPatient();
+
+        assertNotNull(test.toString());
+        assertNotEquals(test.toString(), second.toString());
+        
     }
 }
