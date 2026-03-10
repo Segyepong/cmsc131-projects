a4df4246486f91c618128ffbba9d6e8d2d78902d
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-03-10 11:21:24 -0400

    wrote alert class

--- Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Alert.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Alert.java
@@ -1,5 +1,15 @@
 package edu_fcc_cs;
 
 public class Alert {
-    
+    private Observation observation;
+
+    public Alert(Observation observation) {
+        this.observation = observation;
+    }
+
+    @Override
+    public String toString() {
+
+        return "ALERT: " + observation.toString();
+    }
 }
