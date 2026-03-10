0f7fbc54e7c5b2b64d463d05a678ffa29b8da2ba
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-03-10 09:39:45 -0400

    wrote simulation and simulation_test

--- Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Simulation.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/Simulation.java
@@ -1,5 +1,52 @@
 package edu_fcc_cs;
+import java.util.Random;
 
 public class Simulation {
-    
+    private static Random rand = new Random();
+
+    private Hospital hospital;
+    private int currentTime;
+
+    private int simulationLength = 144;
+
+    public Simulation() {
+
+    }
+
+    public void setup() {
+        hospital = new Hospital(100);
+
+        // create some patients
+        for (int i = 0; i < 10; i++) {
+            hospital.addPatient(Patient.createPatient());
+        }
+
+        currentTime = 0;
+    }
+
+    public void run() {
+
+        while (currentTime < simulationLength) {
+
+            currentTime++;
+
+        }
+    }
+
+    public void process() {
+        System.out.println("Simulation complete.");
+        System.out.println("Total Patients: " + hospital.getPatientCount());
+    }
+
+    public static int getRandomInt(int bound) {
+        return rand.nextInt(bound);
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public int getCurrentTime() {
+        return currentTime;
+    }
 }
--- Projects/Hospital_Simulation/hospital_simulation/src/test/java/edu_fcc_cs/Simulation_test.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/test/java/edu_fcc_cs/Simulation_test.java
@@ -1,11 +1,34 @@
 package edu_fcc_cs;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 public class Simulation_test {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSetupCreatesHospital() {
+
+        Simulation sim = new Simulation();
+        sim.setup();
+
+        assertNotNull(sim.getHospital());
+    }
+
+    @Test
+    public void testRunAdvancesTime() {
+
+        Simulation sim = new Simulation();
+        sim.setup();
+
+        sim.run();
+
+        assertTrue(sim.getCurrentTime() > 0);
+    }
+
+    @Test
+    public void testRandomNumber() {
+
+        int num = Simulation.getRandomInt(10);
+
+        assertTrue(num >= 0 && num < 10);
     }
 }
