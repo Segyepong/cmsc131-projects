45f969136c430f6f86e3e9d5fae50148456cad67
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-03-10 10:28:40 -0400

    wrote call bell device

--- Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/CallBellDevice.java
+++ Projects/Hospital_Simulation/hospital_simulation/src/main/java/edu_fcc_cs/CallBellDevice.java
@@ -1,5 +1,18 @@
 package edu_fcc_cs;
 
-public class CallBellDevice {
-    
+public class CallBellDevice extends Device {
+
+    public CallBellDevice(Patient patient) {
+        super(patient);
+    }
+
+    @Override
+    public Observation poll(int time) {
+
+        if (Simulation.getRandomInt(20) == 0) {
+            return new CallBellObservation(patient, time);
+        }
+
+        return null;
+    }
 }
